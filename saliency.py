a1e2adf858f54971ae1e61652d7b2fe7bc76a22c
gpspelle <gpsunicamp016@gmail.com>
2019-02-04 13:46:37 +0000

    teste

---
+++ saliency.py
@@ -0,0 +1,112 @@
+# Boilerplate imports.
+import tensorflow as tf
+import numpy as np
+import PIL.Image
+from matplotlib import pylab as P
+import pickle
+import os
+slim=tf.contrib.slim
+
+if not os.path.exists('models/research/slim'):
+  os.system("git clone https://github.com/tensorflow/models/")
+old_cwd = os.getcwd()
+os.chdir('models/research/slim')
+from nets import inception_v3
+os.chdir(old_cwd)
+
+# From our repository.
+import saliency
+
+#%matplotlib inline
+
+# Boilerplate methods.
+def ShowImage(im, title='', ax=None):
+  if ax is None:
+    P.figure()
+  P.axis('off')
+  im = ((im + 1) * 127.5).astype(np.uint8)
+  P.imshow(im)
+  P.title(title)
+
+def ShowGrayscaleImage(im, title='', ax=None):
+  if ax is None:
+    P.figure()
+  P.axis('off')
+
+  P.imshow(im, cmap=P.cm.gray, vmin=0, vmax=1)
+  P.title(title)
+
+def ShowDivergingImage(grad, title='', percentile=99, ax=None):  
+  if ax is None:
+    fig, ax = P.subplots()
+  else:
+    fig = ax.figure
+  
+  P.axis('off')
+  divider = make_axes_locatable(ax)
+  cax = divider.append_axes('right', size='5%', pad=0.05)
+  im = ax.imshow(grad, cmap=P.cm.coolwarm, vmin=-1, vmax=1)
+  fig.colorbar(im, cax=cax, orientation='vertical')
+  P.title(title)
+
+def LoadImage(file_path):
+  im = PIL.Image.open(file_path)
+  im = np.asarray(im)
+  return im / 127.5 - 1.0
+
+# Use either wget or curl depending on your OS.
+if not os.path.exists('inception_v3.ckpt'):
+  #os.system("wget http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz")
+  os.system("curl -O http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz")
+  os.system("tar -xvzf inception_v3_2016_08_28.tar.gz")
+
+ckpt_file = './inception_v3.ckpt'
+
+graph = tf.Graph()
+
+with graph.as_default():
+  images = tf.placeholder(tf.float32, shape=(None, 299, 299, 3))
+
+  with slim.arg_scope(inception_v3.inception_v3_arg_scope()):
+    _, end_points = inception_v3.inception_v3(images, is_training=False, num_classes=1001)
+    
+    # Restore the checkpoint
+    sess = tf.Session(graph=graph)
+    saver = tf.train.Saver()
+    saver.restore(sess, ckpt_file)
+    
+  # Construct the scalar neuron tensor.
+  logits = graph.get_tensor_by_name('InceptionV3/Logits/SpatialSqueeze:0')
+  neuron_selector = tf.placeholder(tf.int32)
+  y = logits[0][neuron_selector]
+    
+  # Construct tensor for predictions.
+  prediction = tf.argmax(logits, 1)
+
+# Construct the saliency object. This doesn't yet compute the saliency mask, it just sets up the necessary ops.
+integrated_gradients = saliency.IntegratedGradients(graph, sess, y, images)
+
+# Baseline is a black image.
+baseline = np.zeros(im.shape)
+baseline.fill(-1)
+
+# Compute the vanilla mask and the smoothed mask.
+vanilla_integrated_gradients_mask_3d = integrated_gradients.GetMask(
+  im, feed_dict = {neuron_selector: prediction_class}, x_steps=25, x_baseline=baseline)
+# Smoothed mask for integrated gradients will take a while since we are doing nsamples * nsamples computations.
+smoothgrad_integrated_gradients_mask_3d = integrated_gradients.GetSmoothedMask(
+  im, feed_dict = {neuron_selector: prediction_class}, x_steps=25, x_baseline=baseline)
+
+# Call the visualization methods to convert the 3D tensors to 2D grayscale.
+vanilla_mask_grayscale = saliency.VisualizeImageGrayscale(vanilla_integrated_gradients_mask_3d)
+smoothgrad_mask_grayscale = saliency.VisualizeImageGrayscale(smoothgrad_integrated_gradients_mask_3d)
+
+# Set up matplot lib figures.
+ROWS = 1
+COLS = 2
+UPSCALE_FACTOR = 10
+P.figure(figsize=(ROWS * UPSCALE_FACTOR, COLS * UPSCALE_FACTOR))
+
+# Render the saliency masks.
+ShowGrayscaleImage(vanilla_mask_grayscale, title='Vanilla Integrated Gradients', ax=P.subplot(ROWS, COLS, 1))
+ShowGrayscaleImage(smoothgrad_mask_grayscale, title='Smoothgrad Integrated Gradients', ax=P.subplot(ROWS, COLS, 2))
